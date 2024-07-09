d16eb0525e82cdad78f614d0c2e15b0f1a52a8ac
saptarsidebnath98 <saptarsidebnath666666@gmail.com>
2024-07-09 18:55:08 +0530

    app works finally

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
+import { useState } from "react";
 import RatingPage from "./components/RatingPage";
 import ThankPage from "./components/ThankPage";
 
 function App() {
+  const [rating, setRating] = useState();
+  const [showThanks, setShowThanks] = useState(false);
+
+    function handleClick(e){
+        setRating(e.target.name);
+    }
+
+    function handleSubmit(){
+        setShowThanks(true);
+    }
+
   return (
     <div className="h-screen w-screen bg-VeryDarkBlue flex justify-center items-center">
       <div className="container w-3/12 h-3/6 bg-DarkBlue rounded-3xl shadow-lg px-8 py-8 text-sm font">
-        <RatingPage/>
-        {/* <ThankPage/> */}
+        {showThanks ? 
+        <ThankPage rating={rating}/> : 
+        <RatingPage handleClick={handleClick} handleSubmit={handleSubmit}/>}
+        
+        
       </div>
     </div>
   )
