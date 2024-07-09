946e620e0328f564b036927d06c0ab1ee21ec34f
saptarsidebnath98 <saptarsidebnath666666@gmail.com>
2024-07-09 16:48:09 +0530

    setup done

---
+++ tailwind.config.cjs
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line no-undef
+module.exports =  {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+    colors: {
+    'Orange': 'hsl(25, 97%, 53%)',  
+    'White': 'hsl(0, 0%, 100%)',
+    'LightGrey': 'hsl(217, 12%, 63%)',
+    'DarkBlue': 'hsl(213, 19%, 18%)',
+    'VeryDarkBlue': 'hsl(216, 12%, 8%)',
+    }
+  },
+  plugins: [],
+}
+
