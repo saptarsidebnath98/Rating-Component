f6fd720f6b835bb6d082ac82cbec0d13cbf61869
saptarsidebnath98 <saptarsidebnath666666@gmail.com>
2024-07-09 16:48:52 +0530

    components created

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ function App() {
   return (
     <div className="h-screen w-screen bg-VeryDarkBlue flex justify-center items-center">
       <div className="container w-3/12 h-3/6 bg-DarkBlue rounded-3xl shadow-lg">
-          
+
       </div>
     </div>
   )
